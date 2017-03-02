c304c3d32a01ab0bc2fc8e27e8815c7d2cf0ff64
Sylvain Rojo <Sylvain Rojo>
2017-03-02 18:26:13 +0100

    PotCommun V2

---
+++ LetterGame/src/main/java/fr/esiea/unique/binome/name/dictionary/Dictionary.java
@@ -0,0 +1,52 @@
+package fr.esiea.unique.binome.name.dictionary;
+
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Dictionary implements IDictionary {
+
+    public BufferedReader OpenFile() {
+        String file = "src/main/resources/dico.txt";
+
+        BufferedReader br = null;
+        try {
+            InputStream ips = new FileInputStream(file);
+            InputStreamReader ipsr = new InputStreamReader(ips);
+            br = new BufferedReader(ipsr);
+        }
+        catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        return br;
+    }
+
+    public String isWord(String word) throws IOException {
+        BufferedReader br = this.OpenFile();
+        String currentLine;
+        do {
+            currentLine = br.readLine();
+            if (currentLine.equals(word) || currentLine.equals(null)) {
+                br.close();
+                return currentLine;
+            }
+        } while (br.ready());
+        return "";
+    }
+
+    public boolean containsWord(String word) throws IOException {
+        BufferedReader br = this.OpenFile();
+        String currentLine;
+        do {
+            currentLine = br.readLine();
+            if (currentLine.startsWith(word)) {
+                br.close();
+                return true;
+            }
+        } while (br.ready());
+        return false;
+    }
+}
--- LetterGame/src/main/java/pot/commun/PotCommun.java
+++ LetterGame/src/main/java/pot/commun/PotCommun.java
@@ -2,24 +2,31 @@ package pot.commun;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Dictionary;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.regex.Pattern;
+import java.text.Normalizer;
 
 public class PotCommun {
 
 	private ArrayList<Character> listeDesLettres;
+	private Dictionary dico; 
 	
 	private PotCommun(){
 		this.listeDesLettres = new ArrayList<Character>();
+		this.dico = new Dictionary();
 	}
-	/** Singleton ----------
+	/** Singleton 
 	 * On utilise le singleton pour notre pot commun. De manière à ce que le pot commun
-	 * soit instancié une seule et unique fois.	 * 
+	 * soit instancié une seule et unique fois.	  
 	 * */
+	// Holder du PotCommun
 	private static class PotCommunHolder {
 		private final static PotCommun instance = new PotCommun();
 	}
 	
+	// Le get de l'instance
 	public static PotCommun getInstance() {
 		return PotCommunHolder.instance;
 	}
